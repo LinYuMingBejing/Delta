29bb0540c49bfcd7b128e4f8b6cbdbb1ab37215b
jack-y-lin <jack_y_lin@trendmicro.com>
2024-02-14 21:43:15 +0800

    Add db init steps

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+rm -rf /usr/src/delta/migrations/versions/
+mkdir -p /usr/src/delta/migrations/versions/
+python3 /usr/src/delta/manage.py reset-db-version
 python3 /usr/src/delta/manage.py db migrate
 python3 /usr/src/delta/manage.py db upgrade
--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 import app
 from app import db
+from cmd.reset_db_cmd import ResetDBVersionCommand
+
 from flask_migrate import Migrate, MigrateCommand
 import flask_script as script
 
 app = app.create_application()
 
 manager = script.Manager(app)
+manager.add_command('reset-db-version', ResetDBVersionCommand)
 migrate = Migrate(app=app, db=db)
 
 manager.add_command('db', MigrateCommand)
