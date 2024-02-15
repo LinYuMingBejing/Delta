1d3e77a87f4666f60f6555c838eb96bac40bb170
jack-y-lin <jack_y_lin@trendmicro.com>
2024-02-15 09:57:20 +0800

    Enable ingress

--- infra/minikube/main.tf
+++ infra/minikube/main.tf
@@ -1,4 +1,11 @@
+resource "null_resource" "install_minikube" {
+  provisioner "local-exec" {
+    command = "brew install minikube"
+  }
+}
+
 resource "null_resource" "start_minikube" {
+  depends_on = [null_resource.install_minikube]
   provisioner "local-exec" {
     command = "minikube start"
   }
@@ -20,8 +27,16 @@ resource "null_resource" "install_addons" {
   }
 }
 
+resource "null_resource" "install_ingress" {
+  depends_on = [null_resource.configure_kubeconfig]
+
+  provisioner "local-exec" {
+    command = "minikube addons enable ingress"
+  }
+}
+
 resource "null_resource" "check_minikube" {
-  depends_on = [null_resource.install_addons]
+  depends_on = [null_resource.install_addons, null_resource.install_ingress]
 
   provisioner "local-exec" {
     command = "kubectl wait --for=condition=ready pod -l component=kube-controller-manager -n kube-system --timeout=5m"
